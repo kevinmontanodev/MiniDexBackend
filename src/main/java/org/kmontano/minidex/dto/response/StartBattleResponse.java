2f4b948e2c7bf85cd0ec1b84078f13d7ae0efd57
KevinU20221275 <U20221275@univo.edu.sv>
2026-03-05 19:19:19 -0600

    feat: add StartBattleResponse dto

--- src/main/java/org/kmontano/minidex/dto/response/StartBattleResponse.java
+++ src/main/java/org/kmontano/minidex/dto/response/StartBattleResponse.java
@@ -1,30 +1,27 @@
 package org.kmontano.minidex.dto.response;
 
+import lombok.Data;
 import org.kmontano.minidex.domain.battle.model.BattleContext;
 import org.kmontano.minidex.domain.battle.model.BattleStatus;
-import org.kmontano.minidex.domain.battle.model.BattleTurn;
 import org.kmontano.minidex.dto.shared.BattlePokemon;
 
 import java.util.List;
 
+@Data
 public class StartBattleResponse {
     private String battleId;
     private BattlePokemon currentPlayerPokemon;
     private BattlePokemon currentEnemyPokemon;
     private List<BattlePokemon> playerTeam;
-    private List<BattlePokemon> enemyTeam;
     private BattleStatus status;
-    private BattleTurn turn;
     private String enemyName;
 
-    public StartBattleResponse(String battleId, BattleContext context, String enemyName) {
+    public StartBattleResponse(String battleId, BattleContext context) {
         this.battleId = battleId;
         this.currentPlayerPokemon = context.getPlayer();
         this.currentEnemyPokemon = context.getEnemy();
-        this.playerTeam = context.getPlayerTeam();
-        this.enemyTeam = context.getEnemyTeam();
+        this.playerTeam = context.getPlayerTeam();;
         this.status = context.getStatus();
-        this.turn = context.getCurrentTurn();
-        this.enemyName = enemyName;
+        this.enemyName = "Team Rocket";
     }
 }
