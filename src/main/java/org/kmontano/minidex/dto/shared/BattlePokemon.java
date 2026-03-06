a98ca25d9ac8464cfc4c056abd12a43b250597e8
KevinU20221275 <U20221275@univo.edu.sv>
2026-03-05 19:17:03 -0600

    update: update BattlePokemon dto

--- src/main/java/org/kmontano/minidex/dto/shared/BattlePokemon.java
+++ src/main/java/org/kmontano/minidex/dto/shared/BattlePokemon.java
@@ -1,21 +1,22 @@
 package org.kmontano.minidex.dto.shared;
 
-import org.kmontano.minidex.domain.pokemon.PokemonType;
-import org.kmontano.minidex.domain.pokemon.Move;
-import org.kmontano.minidex.domain.pokemon.Pokemon;
-import org.kmontano.minidex.domain.pokemon.PokemonTypeRef;
+import lombok.Data;
+import org.kmontano.minidex.domain.pokemon.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class BattlePokemon {
     private String pokemonId;
     private String name;
     private int maxHp;
     private int currentHp;
+    private Sprites sprites;
     private int attack;
     private int defense;
     private int speed;
+    private int level;
     private List<Move> moves;
     private List<PokemonType> types;
 
@@ -28,85 +29,16 @@ public class BattlePokemon {
         this.maxHp = p.getStats().getHp();
         this.currentHp = p.getStats().getHp();
         this.attack = p.getStats().getAttack();
+        this.speed = p.getStats().getSpeed();
         this.defense = p.getStats().getDefense();
+        this.level = p.getLevel();
         this.moves = p.getMoves();
         this.types = toPokemonType(p.getTypes());
+        this.sprites = p.getSprites();
     }
 
     public boolean isFainted() {
         return currentHp <= 0;
-    }
-
-    public String getPokemonId() {
-        return pokemonId;
-    }
-
-    public void setPokemonId(String pokemonId) {
-        this.pokemonId = pokemonId;
-    }
-
-    public List<Move> getMoves() {
-        return moves;
-    }
-
-    public void setMoves(List<Move> moves) {
-        this.moves = moves;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public int getDefense() {
-        return defense;
-    }
-
-    public void setDefense(int defense) {
-        this.defense = defense;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public void setAttack(int attack) {
-        this.attack = attack;
-    }
-
-    public int getCurrentHp() {
-        return currentHp;
-    }
-
-    public void setCurrentHp(int currentHp) {
-        this.currentHp = currentHp;
-    }
-
-    public int getMaxHp() {
-        return maxHp;
-    }
-
-    public void setMaxHp(int maxHp) {
-        this.maxHp = maxHp;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<PokemonType> getTypes() {
-        return types;
-    }
-
-    public void setTypes(List<PokemonTypeRef> types) {
-        this.types = toPokemonType(types);
     }
 
     private List<PokemonType> toPokemonType(List<PokemonTypeRef> types){
@@ -116,5 +48,26 @@ public class BattlePokemon {
         }
 
         return pokemonTypes;
+    }
+
+    public BattlePokemon copy() {
+        BattlePokemon copy = new BattlePokemon();
+
+        copy.setPokemonId(this.pokemonId);
+        copy.setName(this.name);
+        copy.setLevel(this.level);
+
+        copy.setMaxHp(this.maxHp);
+        copy.setCurrentHp(this.currentHp);
+
+        copy.setAttack(this.attack);
+        copy.setDefense(this.defense);
+        copy.setSpeed(this.speed);
+
+        copy.setTypes(new ArrayList<>(this.types));
+        copy.setMoves(new ArrayList<>(this.moves));
+        copy.setSprites(this.sprites);
+
+        return copy;
     }
 }
