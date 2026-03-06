b1eb9ecc1a206906a8250585bf9b3e6d006c1498
KevinU20221275 <U20221275@univo.edu.sv>
2026-03-05 19:07:38 -0600

    refactor: move BattleAction interface to action directory

--- src/main/java/org/kmontano/minidex/domain/battle/BattleAction.java
+++
@@ -1,9 +0,0 @@
-package org.kmontano.minidex.domain.battle;
-
-import org.kmontano.minidex.domain.battle.model.BattleContext;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface BattleAction {
-    void execute(BattleContext context);
-}
